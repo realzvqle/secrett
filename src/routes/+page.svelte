65e8dbda1b0e548790e181b29475c2a7dbbc29bf
realzvqle <eyefort@gmail.com>
2024-04-09 13:50:33 -0500

    idk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    body {
+        font-family: "Lucida Console", "Courier New", monospace;
+        text-align: center;
+    }
+</style>
+
+<script>
+
+    let userinput = '';
+    function handleChange(event) {
+        userinput = event.target.value;
+    }
+    function gotoPage(event) {
+        window.location.href = `https://www.google.com/search?q=${userinput}`;
+    }
+</script>
+
+<body>
+    <h2>Google Searcher (I Don't Know Why I Made This)</h2>
+    <input type="text" bind:value={userinput} on:input={handleChange}>
+    <button on:click={gotoPage}>Search</button>
+</body>
